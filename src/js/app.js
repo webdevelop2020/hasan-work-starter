b39985459489fcb971e4e7da194904ede5c5cd55
Hasan Hp Zbook <hasanondev@gmail.com>
2020-04-24 17:14:46 +0400

    intial Commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+class App {
+  constructor () {
+
+  }
+}
+
+export default App;
+
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import App from './app.js';
+
+const app = new App();
+
+// jqurey
+
+const $ = require( "jquery" )( window );
+
+// Vendor Apllication Loader
+
+// import slick from './vendor/slick.js';
+
+// import Tween from './vendor/TweenMax.min.js';
+
+// import Vendor Apps Scripts
+
+// import vendorApp from './vendor/vendor.js';
+
+
